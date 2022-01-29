fb6409c4d3ace7a82ea24422bb0958a71e10ff39
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-01-30 00:24:41 +0900

    シードが隣接にしないようにした

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,12 @@ use wasm_bindgen::prelude::*;
 pub fn generate() -> u32 {
     let local_date = Local::now();
     let salt_env = env!("SALT");
+    let factor_env = env!("FACTOR");
     let salt = salt_env.parse::<u32>().unwrap();
+    let factor = factor_env.parse::<u32>().unwrap();
     let yr = local_date.year() as u32;
     let mo = local_date.month() as u32;
     let dy = local_date.day() as u32;
 
-    yr + mo + dy + salt
+    (yr + mo + dy + salt) * factor
 }
